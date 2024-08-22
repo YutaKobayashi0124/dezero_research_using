b7bba84eb983b8b6aa5666b16cd86a32e8b8fe00
YutaKobayashi <guruda2002@outlook.jp>
2024-08-22 23:02:44 +0900

    layers.pyの変更

--- dezero/layers.py
+++ dezero/layers.py
@@ -146,7 +146,7 @@ class RBF(Layer):
             self._init_C(xp)
 
         # RBF の計算を行う
-        y = F.rbf(x, self.C.data, self.gamma)  # self.C.data に修正
+        y = F.rbf(x, self.C.data, self.gamma.data)  # self.C.data に修正
         return y
 
 
