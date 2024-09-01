0a1767ea925e4e2b9596c3eb1461f107283b4768
YutaKobayashi <guruda2002@outlook.jp>
2024-09-01 14:42:45 +0900

    RBF forwardの変更

--- dezero/functions.py
+++ dezero/functions.py
@@ -310,9 +310,10 @@ class RBF(Function):
 
         # 中心との差分と距離の平方を計算
         diff = x[:, np.newaxis, :] - centers[np.newaxis, :, :]
-        dist_sq = np.sum(diff ** 2, axis=2)
+        dist_sq = F.sum(diff ** 2, axis=2)
+        #dist_sq = np.sum(diff ** 2, axis=2)
         #dist_sq = (diff ** 2).sum(axis=2)
-        rbf = np.exp(-self.beta * dist_sq)
+        rbf = F.exp(-self.beta * dist_sq)
         return rbf
 
     def backward(self, gys):
