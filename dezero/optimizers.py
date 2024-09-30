e1259b38c016f808d2a938f17a325100bf5e118a
YutaKobayashi <guruda2002@outlook.jp>
2024-09-30 20:13:27 +0900

    パラメータの値の保持

--- dezero/optimizers.py
+++ dezero/optimizers.py
@@ -9,6 +9,7 @@ class Optimizer:
     def __init__(self):
         self.target = None
         self.hooks = []
+        self.params_log = []  # パラメータを記録するリスト
 
     def setup(self, target):
         self.target = target
@@ -19,6 +20,10 @@ class Optimizer:
 
         for f in self.hooks:
             f(params)
+
+        # パラメータの値を記録
+        current_params = {f"param_{i}": p.data.copy() for i, p in enumerate(params)}
+        self.params_log.append(current_params)  # パラメータの値をログに保存
 
         for param in params:
             self.update_one(param)
